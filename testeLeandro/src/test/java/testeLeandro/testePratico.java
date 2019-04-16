93e112e9e58d1c51ae8d31ece8bc18483aa093a0
Yaman_Automacao <rodrigo.florindo@yaman.com.br>
2019-04-16 10:36:38 -0300

    Correção Leandro

--- testeLeandro/src/test/java/testeLeandro/testePratico.java
+++ testeLeandro/src/test/java/testeLeandro/testePratico.java
@@ -30,7 +30,7 @@ public class testePratico {
 		
 		navegador.get("https://srbarriga.herokuapp.com/cadastro");
 		navegador.findElement(By.id("nome")).sendKeys("Leandro Brito");
-		navegador.findElement(By.id("email")).sendKeys("brito@gmail.com");
+		navegador.findElement(By.id("email")).sendKeys("brito234@gmail.com");
 		navegador.findElement(By.id("email")).sendKeys("senha1");
 		navegador.findElement(By.xpath("/html/body/div[2]/form/input")).click();
 		org.junit.Assert.assertEquals("Usuário inserido com sucesso", navegador.findElement(By.xpath("/html/body/div[1]")).getText());
@@ -57,7 +57,7 @@ public class testePratico {
 		navegador.findElement(By.xpath("/html/body/div[3]/form/button")).click();
 		navegador.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[2]/a")).click();
 		navegador.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[2]/ul/li[1]/a")).click();
-		navegador.findElement(By.id("nome")).sendKeys("Leandro");
+		navegador.findElement(By.id("nome")).sendKeys("Leandro23");
 		navegador.findElement(By.xpath("/html/body/div[2]/form/div[2]/button")).click();
 		org.junit.Assert.assertEquals("Conta adicionada com sucesso", navegador.findElement(By.xpath("/html/body/div[1]")).getText());
 		navegador.findElement(By.xpath("//*[@id=\"navbar\"]/ul/li[5]/a")).click();
